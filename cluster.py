deba8b9c52e2b45d4d7d84433f14abdd30f64d1a
Jim <jim.lemmers@gmail.com>
2017-12-10 18:56:22 +0100

    refactored the algorithm into a class and moved the testing code to test.py

--- cluster.py
+++ cluster.py
@@ -1,108 +1,131 @@
 import numpy as np
-import pandas as pd
 import random
 from collections import Counter
+import matplotlib.pyplot as plt
 
 
-df = pd.read_csv('Data_fuzzy-shrooms.csv')
-# X = pd.read_csv('https://raw.githubusercontent.com/medhini/Genetic-Algorithm-Fuzzy-K-Modes/master/zoo.csv')
-y = df['class'].values
-X = df
-k = 5
-m = 1.2
-n_iter = 10
-p = len(X.columns)
-n = len(X)
-columns = X.columns
+class NaNException(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-def random_numbers(x, attribute):
-    xs = x[attribute].unique()
-    randoms = np.random.dirichlet(np.ones(len(xs)), size=1)
-    return list(zip(xs, randoms[0]))
+class FuzzyKmodes(object):
+    def __init__(self, X, y=None, k=2, m=1.5, n_iter=10, error=0.00005):
+        self.X = X
+        self.y = y
+        self.k = k
+        self.m = m
+        self.n_iter = n_iter
+        self.p = len(X.columns)
+        self.n = len(X)
+        self.columns = X.columns
+        self.u = None
+        self.clusters = None
+        self.exp = 1 / ( self.m - 1 )
+        self.error = error
 
-def init(x, k):
-    return [[random_numbers(x, attribute)
-            for attribute in columns]
-            for _ in range(k)]
+    def random_numbers(self, values):
+        randoms = np.random.dirichlet(np.ones(len(values)), size=1)
+        return list(zip(values, randoms[0]))
 
-def dissimilarity(fuzzy_set, x_l):
-    return sum(0 if value == x_l else confidence
-               for value, confidence in fuzzy_set)
+    def init(self, x, k):
+        return [[self.random_numbers(x[attribute].unique())
+                for attribute in self.columns]
+                for _ in range(self.k)]
 
-def distance(cluster, x):
-    return sum(dissimilarity(fuzzy_set, x[j])
-               for j, fuzzy_set in enumerate(cluster))
+    def dissimilarity(self, fuzzy_set, value):
+        return sum(0 if term == value else confidence
+                for term, confidence in fuzzy_set)
 
-def membership(x, cluster, clusters):
-    exp = 1 / ( m - 1 )
-    membership = sum((distance(cluster, x) / distance(other_cluster, x))** exp
-               # if cluster != other_cluster else 1
-               for other_cluster in clusters)
-    return membership ** -1
+    def distance(self, cluster, row):
+        return sum(self.dissimilarity(fuzzy_set, row[column])
+                for column, fuzzy_set in enumerate(cluster))
 
-def gamma(x_j, attribute, membership):
-    if x_j == attribute:
-        return membership ** m
-    else:
-        return 0
+    def membership(self, row, cluster, clusters):
+        membership = sum((self.distance(cluster, row) /
+                          self.distance(other_cluster, row))** self.exp
+                         for other_cluster in clusters)
+        if np.isnan(membership):
+            return 0
+        return membership ** -1
 
-def certainty(Xj, value, u_row):
-    new_memb = sum(u_row[i]**m if x == value else 0 for i, x in enumerate(Xj))
-    return new_memb
+    def certainty(self, column, value, cluster_u):
+        new_memb = sum(cluster_u[row] ** self.m if x == value else 0
+                       for row, x in enumerate(column))
+        return new_memb
 
-def update_clusters(clusters, X, u):
-    # Update the clusters
-    for j, cluster in enumerate(clusters):
-        cl_u = u[j]
-        for l, attribute_membership in enumerate(cluster):
-            feature = columns[l]
-            clusters[j][l] = [(value, certainty(X[feature].values, value, cl_u))
-                              for value, _ in attribute_membership]
-    return clusters
+    def update_clusters(self, clusters, X, u):
+        for j, cluster in enumerate(clusters):
+            cluster_u = u[j]
+            for l, term_membership in enumerate(cluster):
+                feature = self.columns[l]
+                column = X[feature].values
+                clusters[j][l] = [(term, self.certainty(column, term, cluster_u))
+                                for term, _ in term_membership]
+        return clusters
 
-def check_normal(clusters, iter):
-    clusters = [[normalize(row) for row in cluster]
-                for cluster in clusters]
-    for cluster in clusters:
-        for row in cluster:
-            if not (0.9 < sum(r[1] for r in row) < 1.1):
-                print('error', row)
-                print('error', sum(r[1] for r in row))
-                raise Exception(str(iter) + str(cluster))
-    return clusters
+    def check_normal(self, clusters, iter):
+        clusters = [[self.normalize(row) for row in cluster]
+                    for cluster in clusters]
+        for cluster in clusters:
+            for row in cluster:
+                if not (0.9 < sum(r[1] for r in row) < 1.1):
+                    print('error', row)
+                    print('error', sum(r[1] for r in row))
+                    raise NaNException(str(iter) + str(cluster))
+        return clusters
 
-def normalize(row):
-    summed = sum(r[1] for r in row)
-    return [(value, x/summed) for value, x in row]
+    def normalize(self, row):
+        summed = sum(r[1] for r in row)
+        return [(value, x / summed) for value, x in row]
 
-def main():
-    clusters = init(X, k) # Step 1
-    clusters = check_normal(clusters, -2)
-    u = [[membership(x, cluster, clusters) for x in X.values] # Step 2
-        for cluster in clusters]
-    clusters = update_clusters(clusters, X, u) # Step 3
-    clusters = check_normal(clusters, -1)
+    def cluster(self):
+        clusters = self.init(self.X, self.k)
+        clusters = self.check_normal(clusters, -2)
+        u = [[self.membership(row, cluster, clusters) for row in self.X.values]
+            for cluster in clusters]
+        clusters = self.update_clusters(clusters, self.X, u)
+        clusters = self.check_normal(clusters, -1)
 
-    for _ in range(n_iter):
-        new_u = [[membership(x, cluster, clusters) for x in X.values] # Step 2
-                for cluster in clusters]
-        clusters_new = update_clusters(clusters, X, new_u) # Step 3
-        clusters_new = check_normal(clusters_new, _)
-        u_error = np.mean([abs(m - new_m) for i in range(k)
-                    for m, new_m in zip(u[i], new_u[i])])
-        print(_)
-        print('error', u_error)
-        # print(clusters)
-        u = new_u
-        clusters = clusters_new
+        self.iteration = 0
+        while self.iteration < self.n_iter or u_error > self.error:
+            try:
+                new_u = [[self.membership(x, cluster, clusters) for x in
+                        self.X.values]
+                        for cluster in clusters]
+                clusters_new = self.update_clusters(clusters, self.X, new_u)
+                clusters_new = self.check_normal(clusters_new, self.iteration)
+                u_error = np.mean([abs(m - new_m) for i in range(self.k)
+                            for m, new_m in zip(u[i], new_u[i])])
+                # print(clusters)
+                u = new_u
+                clusters = clusters_new
+                self.iteration += 1
+            except NaNException:
+                pass
+        self.u = u
+        self.clusters = clusters
 
-    cluster_membership = {i: list() for i in range(k)}
-    for i in range(n):
-        cluster = max((u[cl][i], cl) for cl in range(k))[-1]
-        cluster_membership[cluster].append(y[i])
+    def cluster_membership(self):
+        cluster_membership = {i: list() for i in range(self.k)}
+        for i in range(self.n):
+            cluster = max((self.u[cl][i], cl) for cl in range(self.k))[-1]
+            if type(self.y) is not None:
+                value = self.y[i]
+            else:
+                value = i
+            cluster_membership[cluster].append(value)
+        return cluster_membership
 
-    for cl, vals in cluster_membership.items():
-        print(cl, Counter(vals))
+    def count_values(self, cluster_membership):
+        return {cl: Counter(vals) for cl, vals in cluster_membership.items()}
 
-if __name__ == '__main__':
-    main()
+    def plot_clusters(self):
+        cluster_membership = self.cluster_membership()
+        cluster_membershp = {cl: Counter(vals)
+                             for cl, vals in cluster_membership.items()}
+        for cl, vals in cluster_membership.items():
+            plt.subplot(1, self.k, cl+ 1 )
+            if vals:
+                # Filter out the NaN values.
+                plt.hist([v for v in vals if v ==v])
+        plt.show()
